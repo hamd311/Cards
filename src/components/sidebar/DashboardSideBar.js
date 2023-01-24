e36d221c4a59d8e1282fe64070d54f97bfdbe1cb
Muzamil Khan <muzamilkhan111814@gmail.com>
2023-01-25 00:04:52 +0500

    update sidebar issue

--- src/components/sidebar/DashboardSideBar.js
+++ src/components/sidebar/DashboardSideBar.js
@@ -1,52 +1,53 @@
-import React,{useState}from 'react'
+import React, { useState } from "react";
 import logo from "../../assets/images/logo.svg";
 import { useNavigate } from "react-router-dom";
-import Navitem from './Navitem';
+import Navitem from "./Navitem";
 
 import card from "../../assets/icons/card.svg";
 import users from "../../assets/icons/users.svg";
 
-
-
-
 const DashboardSideBar = (props) => {
-
-    const [navListStatus, setNavListStatus] = useState(false);
+  const [navListStatus, setNavListStatus] = useState(false);
   const [style, setStyle] = useState("hidden");
-    const navigate = useNavigate();
-    let cardActive = false;
-    let userActive = false;
+  const navigate = useNavigate();
+  let cardActive = false;
+  let userActive = false;
 
-    if (props.active === 1) {
-        cardActive = true;
-      } else if (props.active === 2) {
-        userActive = true;
-      }
+  if (props.active === 1) {
+    cardActive = true;
+  } else if (props.active === 2) {
+    userActive = true;
+  }
 
-      const toggleNavList = () => {
-        if (navListStatus) {
-          setNavListStatus(false);
-          setStyle("transition-transform	 duration-300 ease-in hidden");
-        } else {
-          setNavListStatus(true);
-          setStyle(" transition	 duration-300 ease-in");
-        }
-      };
+  const toggleNavList = () => {
+    if (navListStatus) {
+      setNavListStatus(false);
+      setStyle("transition-transform	 duration-300 ease-in hidden");
+    } else {
+      setNavListStatus(true);
+      setStyle(" transition	 duration-300 ease-in");
+    }
+  };
   return (
-      <div  className='bg-black p-8 w-full h-full  flex flex-col '
-     
-      >
+    <div
+      className="bg-black p-8 w-full h-full  flex flex-col "
+      style={{
+        backgroundColor: "black",
+        position: "fixed",
+        zIndex: 10,
+        width: "17%",
+      }}
+    >
+      <img
+        src={logo}
+        alt="logo"
+        className="cursor-pointer"
+        onClick={() => {
+          navigate("/");
+        }}
+      />
 
-<img
-          src={logo}
-          alt="logo"
-          className="cursor-pointer"
-          onClick={() => {
-            navigate("/");
-          }}
-        />
-
-<ul className={`text-white mt-10 lg:block  w-full`}>
+      <ul className={`text-white mt-10 lg:block  w-full`}>
         <li className="my-5 ">
           <Navitem text="Cards" icon={card} link={"/"} active={cardActive} />
         </li>
@@ -60,10 +61,8 @@ const DashboardSideBar = (props) => {
           />
         </li>
       </ul>
+    </div>
+  );
+};
 
-      </div>
-    
-  )
-}
-
-export default DashboardSideBar
+export default DashboardSideBar;
